228a9d73fd54ec832c8482966957948f592fe049
allfro <ndouba@gmail.com>
2016-03-07 21:10:57 -0500

    ENH: added source connection param to collector

--- src/oobre/collector/httprequest.py
+++ src/oobre/collector/httprequest.py
@@ -2,7 +2,7 @@ from twisted.web.resource import Resource
 from twisted.web.server import Site
 from urllib import unquote_plus
 
-from oobre.collector import Collector
+from oobre.conntrack import get_connection
 
 
 __author__ = 'Nadeem Douba'
@@ -23,17 +23,23 @@ class CollectorProtocol(Resource):
         Resource.__init__(self)
         self.collector = collector_class()
 
-    def render_DO(self, request):
-        self.collector.dataReceived(unquote_plus(request.uri.lstrip('/')))
+    def _render(self, request):
+        self.collector.dataReceived(request.channel._connection,
+                                    unquote_plus(request.uri.lstrip('/')))
         request.setHeader('Server', 'Apache/2.4.7 (Windows)')
         return '<html><body><h1>It works!</h1></body></html>'
 
     def __getattr__(self, item):
         if item.startswith('render_'):
-            return self.render_DO
+            return self._render
 
 
 class CollectorProtocolFactory(Site):
 
     def __init__(self, collector_class):
         Site.__init__(self, CollectorProtocol(collector_class))
+
+    def buildProtocol(self, addr):
+        proto = Site.buildProtocol(self, addr)
+        proto._connection = get_connection(addr)
+        return proto
