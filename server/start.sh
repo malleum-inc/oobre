ed9ae220f4462e84ad1de9bdf7beab4a25e5d9fc
allfro <ndouba@gmail.com>
2016-03-08 21:45:29 -0500

    ENH: added SqlLiteCollector

--- server/start.sh
+++ server/start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 echo 'Starting OOBRE... '
+export PYTHONPATH=../examples:../src
 twistd -y oobre.tac
 echo 'done.'
--- src/oobre/collector/__init__.py
+++ src/oobre/collector/__init__.py
@@ -1,3 +1,6 @@
+import sqlite3
+from oobre.config import config
+
 __author__ = 'Nadeem Douba'
 __copyright__ = 'Copyright 2012, OOBRE Project'
 __credits__ = []
@@ -13,3 +16,25 @@ class Collector(object):
 
     def dataReceived(self, source, data):
         raise NotImplementedError("Please implement this method.")
+
+
+class SqliteCollector(Collector):
+
+    create_table_statement = """CREATE TABLE IF NOT EXISTS data(
+                                    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    src VARCHAR(15),
+                                    sport INTEGER,
+                                    dst VARCHAR(15),
+                                    dport INTEGER,
+                                    data BLOB);"""
+
+    def __init__(self, name):
+        self.db = sqlite3.connect(config.get(name, 'db_file'))
+        self.db.execute(self.create_table_statement)
+
+    def dataReceived(self, connection, data):
+        self.db.execute(
+            "INSERT INTO data(src, sport, dst, dport, data) VALUES (?,?,?,?,?);",
+            (connection.src, connection.sport, connection.dst, connection.dport, data)
+        )
+        self.db.commit()
